a37a6d10fecf371a71e67cc93ee87364cc9cc945
sametmax <lesametlemax@gmail.com>
2012-10-15 01:13:20 +0200

    Fixing __init__

--- batbelt/__init__.py
+++ batbelt/__init__.py
@@ -4,6 +4,6 @@
 
 
 from strings import slugify, normalize
-from structs import chunk, get, dmerge
+from structs import chunks, get, dmerge
 from objects import attr, import_from_path
 from parallel import process, thread
